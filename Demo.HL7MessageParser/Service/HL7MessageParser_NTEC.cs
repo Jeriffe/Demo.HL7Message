2b968f5f32c63224750123d342803a2f22e0fb9c
Water Yue <wyue@touchpointmed.com>
2020-03-16 14:43:08 +0800

    improve code

--- Demo.HL7MessageParser/Service/HL7MessageParser_NTEC.cs
+++ Demo.HL7MessageParser/Service/HL7MessageParser_NTEC.cs
@@ -179,7 +179,7 @@ namespace Demo.HL7MessageParser
                 mdsResult.hasMdsAlert = false;
                 mdsResult.errorDesc = "System cannot perform Allergy, AlertProfile is empty.";
                 //here should use drug name, not drugItemCode
-                return GenerateFinalResultFromMDSResult(mdsResult, drugItemCode);
+                return mdsResult.ToConvert(drugItemCode);
             }
 
             #region check if need MDS check
@@ -187,7 +187,7 @@ namespace Demo.HL7MessageParser
             if (!checkMDS.CheckDrugCodeIfNeedMDSCheck(drugItemCode, ref mdsResult))
             {
                 //here should use drug name, not drugItemCode
-                return GenerateFinalResultFromMDSResult(mdsResult, drugItemCode);
+                return mdsResult.ToConvert(drugItemCode);
             }
             /*****2.5.3 2: ADR record (1.4.2) if its severity is “Mild”, not perform MDS checking for current ADR profile*****/
             checkMDS.CheckADRProfileForMDSCheck(ref alertProfileRes, ref mdsResult);
@@ -211,7 +211,7 @@ namespace Demo.HL7MessageParser
                     hasDrugError = true
                 };
                 //here should use drug name, not drugItemCode
-                return GenerateFinalResultFromMDSResult(mdsResult, drugItemCode);
+                return mdsResult.ToConvert(drugItemCode);
             }
 
             var drugProperty = getDrugMdsPropertyHqRes.Return[0].DrugProperty;
@@ -243,7 +243,7 @@ namespace Demo.HL7MessageParser
                     errorDesc = "System cannot perform Allergy, Drug Preparation Response is empty.",
                     hasDrugError = true
                 };
-                return GenerateFinalResultFromMDSResult(mdsResult, drugProperty.Displayname);
+                return mdsResult.ToConvert(drugProperty.Displayname);
             }
             
             if (FullCacheHK.DrugMasterCache[drugItemCode] == null)
@@ -260,134 +260,7 @@ namespace Demo.HL7MessageParser
             string drugName = drugProperty.Displayname;
             mdsResult = CheckDrugClass(patientEnquiry, alertProfileRes, getDrugMdsPropertyHqRes, getPreparationRes, ref drugName);
 
-            return GenerateFinalResultFromMDSResult(mdsResult, drugName);
-        }
-        private MdsCheckFinalResult GenerateFinalResultFromMDSResult(MDSCheckResult mdsResult, string drugName)
-        {
-            MdsCheckFinalResult resultForShow = new MdsCheckFinalResult();
-            resultForShow.DrugName = drugName;
-            #region system error
-            if (false == string.IsNullOrEmpty(mdsResult.errorDesc))
-            {
-                resultForShow.SystemErrorMessage += mdsResult.errorDesc + Environment.NewLine;
-            }
-            if (mdsResult.allergyError != null && mdsResult.allergyError.hasAllergyError)
-            {
-                resultForShow.SystemErrorMessage += mdsResult.allergyError.errorDesc + "\r\n" +
-                    (string.IsNullOrEmpty(mdsResult.allergyError.errorCause) ? string.Empty : mdsResult.allergyError.errorCause + "\r\n") +
-                    (string.IsNullOrEmpty(mdsResult.allergyError.errorAction) ? string.Empty : mdsResult.allergyError.errorAction) +
-                    Environment.NewLine;
-            }
-            if (mdsResult.adrError != null && mdsResult.adrError.hasAdrError)
-            {
-                resultForShow.SystemErrorMessage += mdsResult.adrError.errorDesc + "\r\n" +
-                    (string.IsNullOrEmpty(mdsResult.adrError.errorCause) ? string.Empty : mdsResult.adrError.errorCause + "\r\n") +
-                    (string.IsNullOrEmpty(mdsResult.adrError.errorAction) ? string.Empty : mdsResult.adrError.errorAction + "\r\n") +
-                    Environment.NewLine;
-            }
-            if (mdsResult.drugError != null && mdsResult.drugError.hasDrugError)
-            {
-                resultForShow.SystemErrorMessage += mdsResult.drugError.errorDesc + "\r\n" +
-                    (string.IsNullOrEmpty(mdsResult.drugError.errorCause) ? string.Empty : mdsResult.drugError.errorCause + "\r\n") +
-                    (string.IsNullOrEmpty(mdsResult.drugError.errorAction) ? string.Empty : mdsResult.drugError.errorAction + "\r\n") +
-                    Environment.NewLine;
-            }
-            #endregion
-
-            #region allergy alert message
-            if (mdsResult.drugAllergyCheckingResults !=null && mdsResult.drugAllergyCheckingResults.hasDrugAllergyAlert)
-            {
-                resultForShow.HasMdsAlert = true;
-                foreach (var allergyMsg in mdsResult.drugAllergyCheckingResults.drugAllergyAlerts)
-                {
-                    /*
-                    1 Clinical Manifestations will be delimited by semicolon ";", if the last clinical manifestation is equal to additional information, system should hide the additional information field.
-                    2 System should reconstruct the alert message if the following keyword is found in the message:
-                    Sequence of keyword matching	If the following keywords found in [drugAllergyAlertMessages]	Replace the message by the following message
-                    1	an allergic/a cross-sensitivity	Use of uppercase[drugDdimDisplayName from 2.5.1] may result in allergic/cross-sensitivity reaction.
-                    2	an idiosyncratic	Use of uppercase[drugDdimDisplayName from 2.5.1] may result in idiosyncratic reaction.
-                    3	an allergic	Use of uppercase[drugDdimDisplayName from 2.5.1] may result in allergic reaction.
-                    4	a cross-sensitivity	Use of uppercase[drugDdimDisplayName from 2.5.1] may result in cross-sensitivity reaction.                     
-                     */
-                    if (allergyMsg.drugAllergyAlertMessage.Contains("an allergic/a cross-sensitivity"))
-                    {
-                        allergyMsg.drugAllergyAlertMessage = drugName + " may result in allergic/cross-sensitivity reaction.";
-                    }
-                    else if (allergyMsg.drugAllergyAlertMessage.Contains("an idiosyncratic"))
-                    {
-                        allergyMsg.drugAllergyAlertMessage = drugName + " may result in idiosyncratic reaction.";
-                    }
-                    else if (allergyMsg.drugAllergyAlertMessage.Contains("an allergic"))
-                    {
-                        allergyMsg.drugAllergyAlertMessage = drugName + " may result in allergic reaction.";
-                    }
-                    else if (allergyMsg.drugAllergyAlertMessage.Contains("a cross-sensitivity"))
-                    {
-                        allergyMsg.drugAllergyAlertMessage = drugName + " may result in cross-sensitivity reaction.";
-                    }
-
-                    if (allergyMsg.manifestation.EndsWith(";")) allergyMsg.manifestation.TrimEnd(new char[] { ';' });
-
-                    resultForShow.MdsCheckAlertDetails.Add(new MdsCheckAlert("Allergy Checking",
-                            allergyMsg.allergen + " - Allergy history reported" + "\r\n" +
-                            "Clinical Manifestation: " + allergyMsg.manifestation + "\r\n" +
-                            "Additional Information: " + allergyMsg.remark + "\r\n" +
-                            "Level of Certainty: " + allergyMsg.certainty + "\r\n" +
-                            allergyMsg.drugAllergyAlertMessage));
-                }
-            }
-            #endregion
-
-            #region ddcm alert (G6PD alert)
-            if (mdsResult.ddcmCheckingResults != null && mdsResult.ddcmCheckingResults.hasDdcmAlert)
-            {
-                foreach (string ddcmAlert in mdsResult.ddcmCheckingResults.ddcmAlertMessages)
-                {
-                    resultForShow.MdsCheckAlertDetails.Add(
-                        new MdsCheckAlert(
-                                            "G6PD Deficiency Contraindication Checking",
-                                            ddcmAlert
-                                         )
-                        );
-                }
-            }
-            #endregion
-
-            #region adr alert
-            if (mdsResult.drugAdrCheckingResults != null && mdsResult.drugAdrCheckingResults.hasDrugAdrAlert)
-            {
-                foreach (DrugAdrAlert adrAlert in mdsResult.drugAdrCheckingResults.drugAdrAlerts)
-                {
-                    if (adrAlert.reaction.EndsWith(";")) adrAlert.reaction.TrimEnd(new char[] { ';' });
-
-                    if (adrAlert.drugAdrAlertMessage.Contains("an allergic/a cross-sensitivity"))
-                    {
-                        adrAlert.drugAdrAlertMessage = drugName + " may result in adverse drug/cross-sensitivity reaction.";
-                    }
-                    else if (adrAlert.drugAdrAlertMessage.Contains("an idiosyncratic"))
-                    {
-                        adrAlert.drugAdrAlertMessage = drugName + " may result in idiosyncratic reaction.";
-                    }
-                    else if (adrAlert.drugAdrAlertMessage.Contains("an allergic"))
-                    {
-                        adrAlert.drugAdrAlertMessage = drugName + " may result in adverse drug reaction.";
-                    }
-                    else if (adrAlert.drugAdrAlertMessage.Contains("a cross-sensitivity"))
-                    {
-                        adrAlert.drugAdrAlertMessage = drugName + " may result in cross-sensitivity reaction.";
-                    }
-                    resultForShow.MdsCheckAlertDetails.Add(new MdsCheckAlert("Adverse Drug Reaction Checking",
-                        drugName + " - Adverse drug reaction history reported" + "\r\n" +
-                        "Adverse Drug Reaction: " + adrAlert.reaction + "\r\n" +
-                        "Additional Information: " + adrAlert.remark + "\r\n" +
-                        "Level of Severity: " + adrAlert.severity + "\r\n" +
-                        adrAlert.drugAdrAlertMessage
-                        ));
-                }
-            }
-            #endregion
-
-            return resultForShow;
+            return mdsResult.ToConvert(drugName);
         }
         
         private MDSCheckResult CheckDrugClass(PatientDemoEnquiry patientEnquiry,
